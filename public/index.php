7051b960d407236f74c9cd68d1e1d9495513ed81
hong <2143@11.com>
2023-11-14 15:24:19 +0800

    无

--- public/index.php
+++ public/index.php
@@ -14,23 +14,33 @@ if(isset($_GET['cx'])){
 			// 要提交的数据
 			$data = array('ApiRequest' => $_GET['ApiRequest']);
 
-			// 初始化 curl 对象
-			$ch = curl_init();
-
-			// 设置请求选项
-			curl_setopt($ch, CURLOPT_URL, 'http://global-tide.nmdis.org.cn/Api/Service.ashx');
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-			// 执行请求并获取返回的数据
-			$response = curl_exec($ch);
-
-			// 关闭 curl 对象
-			curl_close($ch);
-
-			// 处理返回的数据
+			$curl = curl_init();
+				
+			curl_setopt_array($curl, array(
+				   CURLOPT_URL => 'https://global-tide.nmdis.org.cn/Api/Service.ashx',
+				   CURLOPT_RETURNTRANSFER => true,
+				   CURLOPT_ENCODING => '',
+				   CURLOPT_MAXREDIRS => 10,
+				   CURLOPT_TIMEOUT => 0,
+				   CURLOPT_FOLLOWLOCATION => true,
+				   CURLOPT_CUSTOMREQUEST => 'POST',
+				   CURLOPT_POSTFIELDS => $data,
+				   CURLOPT_HTTPHEADER => array(
+					  'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+					  'Accept: */*',
+					  'Host: global-tide.nmdis.org.cn',
+					  'Connection: keep-alive',
+					  'Content-Type: multipart/form-data;',
+					  'Cookie: ASP.NET_SessionId=dt3bh3sejcy31b2q55ynel3x'
+				   ),
+			));
+				
+			$response = curl_exec($curl);
+				
+			curl_close($curl);
 			echo $response;
+				
+
 		}else{
 			// 要请求的 URL
 			$url = 'https://api.openweathermap.org/data/2.5/weather?lon='.$_GET['lon'].'&lat='.$_GET['lat'].'&appid=dfd932ab2b1ee2b826b854aea12e6713&lang=zh_cn';
